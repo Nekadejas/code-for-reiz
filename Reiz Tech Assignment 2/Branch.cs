41ca7d355304772907bc75e8dea21c0933e1fefa
Gediminas Zamulskis <g.zamulskis@gmail.com>
2023-02-04 11:52:03 +0200

    Implemented task

---
+++ Reiz Tech Assignment 2/Branch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Reiz_Tech_Assignment_2
+{
+    internal class Branch
+    {
+        protected List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+        public void PushBranch(Branch branch) 
+        {
+            branches.Add(branch);
+        }
+        
+        public int GetTreeDepth(Branch branch, int depth) 
+        {
+            int result = depth + 1;
+            
+            foreach (Branch item in branch.branches)
+            {
+                    result = Math.Max(result, GetTreeDepth(item, depth +1));
+            }
+
+            return result;
+
+        }
+        
+    }
+}
---
+++ Reiz Tech Assignment 2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Reiz_Tech_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Branch();
+            var branch1 = new Branch();
+            var branch11 = new Branch();
+            var branch2 = new Branch();
+            var branch21 = new Branch();
+            var branch211 = new Branch();
+            var branch22 = new Branch();
+            var branch221 = new Branch();
+            var branch2211 = new Branch();
+            var branch222 = new Branch();
+            var branch23 = new Branch();
+            var branch22111 = new Branch();
+
+            root.PushBranch(branch1);
+            root.PushBranch(branch2);
+            branch1.PushBranch(branch11);
+            branch2.PushBranch(branch21);
+            branch2.PushBranch(branch22);
+            branch2.PushBranch(branch23);
+            branch21.PushBranch(branch211);
+            branch22.PushBranch(branch221);
+            branch22.PushBranch(branch222);
+            branch211.PushBranch(branch2211);
+            branch2211.PushBranch(branch22111);
+
+
+
+
+
+
+            Console.WriteLine(root.GetTreeDepth(root, 0));
+
+            
+
+        }
+    }
+}
